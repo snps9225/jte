d052b005aa67e3e211b7731b9b7ea7c8cf7adee1
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-20 12:13:55 -0400

    Update withGit.groovy
    Edit Git Lib

--- libraries/git/steps/withGit.groovy
+++ libraries/git/steps/withGit.groovy
@@ -1,26 +1,20 @@
 
-void call(Map args){
-
+void call(){
+  stage("Git: Checkout")  {
+  String url = config.git_url
+  String cred = config.git_cred
+  
   // check required parameters
-  if (!args.url || !args.cred)
+  if (!url || !cred)
     error """
     withGit syntax error.
     Input Parameters:
       url: https git url to repository (required)
       cred: jenkins credential ID for github. (required)
-      branch: branch in the repository to checkout. defaults to master. (optional)
     """
 
-  withCredentials([usernamePassword(credentialsId: args.cred, passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    repo = args.url.split("/").last() - ".git"
-    withEnv(["git_url_with_creds=${args.url.replaceFirst("://","://${USER}:${PASS}@")}"]) {
-      node {
-        sh "rm -rf ${repo}"
-        sh "set +x && git clone ${env.git_url_with_creds}"
-        dir(repo){
-          sh "git checkout ${args.branch ?: "master"}"
-        }
-      }
-    }
+   steps {
+      sh "git credentialsId: '${cred}', url: '${url}'"
+   }
   }
 }
