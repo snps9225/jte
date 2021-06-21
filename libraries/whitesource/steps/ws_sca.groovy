2ef7f458560d9ddb37b41aca1a328ada50fa3f68
Karn Chahar <karn@synopsys.com>
2021-06-21 10:43:15 -0400

    Add Steps

--- libraries/whitesource/steps/ws_sca.groovy
+++ libraries/whitesource/steps/ws_sca.groovy
@@ -1,7 +1,18 @@
 void call(){
-    stage("WhiteSource: Software Composition Analysis"){
-	String product = config.ws_product
-        String project = config.ws_project
-	println "SCA of ${product}/${project} from WS library"
+    node{
+		
+    	stage("WhiteSource: Software Composition Analysis"){
+		String product = config.ws_product
+        	String project = config.ws_project
+		String configs = resource(config.ws_config)
+	
+		sh "echo ${resource(config.ws_config)}"	
+		sh "echo \"Test script\" > config1"
+		sh "cat ${configs}  >> configs1"
+		println "Contents of config1: "
+		sh "echo config1"
+		println "SCA of ${product}/${project} from WS library"
+		sh 
+    	}
     }
 }
