bad81e9db0edcda2914d916633eeacfe53288df2
Karn Chahar <karn@synopsys.com>
2021-06-21 11:41:07 -0400

    Add Steps

--- libraries/whitesource/steps/ws_sca.groovy
+++ libraries/whitesource/steps/ws_sca.groovy
@@ -1,16 +1,25 @@
 void call(){
     node{
 		
-    	stage("WhiteSource: Software Composition Analysis"){
+    	stage("WhiteSource: Download WS Agent"){
 		String product = config.ws_product
         	String project = config.ws_project
 		String configs = resource(config.ws_config)
 		String options = config.ws_cli
 	
-		sh "echo \"Test Script: 2\" > config"
-		sh "echo \"${configs}\" >> config"
-		sh "cat config"
-		println "SCA of ${product}/${project} from WS library"
+		sh 'curl -LO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar > wss-unified-agent.jar'
+  		sh 'curl -LO https://github.com/whitesource/unified-agent-distribution/raw/master/standAlone/wss-unified-agent.config > wss-unified-agent.config'
+		
+		sh "chmod +x ./wss-unified-agent.config"
+		sh "echo \"${configs}\" >> wss-unified-agent.config"
+		sh "cat wss-unified-agent.config"
+		println "SCA of ${product}/${project}:"
+	}
+
+	stage("WhiteSource: Run Unified Agent"){
+		sh "echo \"#!/bin/bash\" > wss_agent.sh"
+    		sh "echo \"java -jar wss-unified-agent.jar \"$@\"\" >> wss_agent.sh"  
+    		sh "chmod +x ./wss_agent.sh"
 		println "s ./wss_agent.sh -apiKey -userKey -product ${product} -project ${project} -wss.url https://app.whitesourcesoftware.com/agent -d ./. ${options}"
     	}
     }
