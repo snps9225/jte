c39dbdc71f360caa7502e297a8a02853af7c57bb
Karn Chahar <karn@synopsys.com>
2021-06-21 12:05:21 -0400

    Add Steps

--- libraries/whitesource/steps/ws_sca.groovy
+++ libraries/whitesource/steps/ws_sca.groovy
@@ -6,7 +6,7 @@ void call(){
         	String project = config.ws_project
 		String configs = resource(config.ws_config)
 		String options = config.ws_cli
-		String agent   = resource("wss_agent")
+		String agent   = resource("wss_agent.txt")
 	
 		sh 'curl -LO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar > wss-unified-agent.jar'
   		//sh 'curl -LO https://github.com/whitesource/unified-agent-distribution/raw/master/standAlone/wss-unified-agent.config > wss-unified-agent.config'
@@ -18,8 +18,8 @@ void call(){
 	}
 
 	stage("WhiteSource: Run Unified Agent"){
-		sh "cat ${agent}"
-		//sh "echo \"${agent}\" > wss_agent.sh"
+		println "Agent: ${agent}"
+		sh "echo \"${agent}\" > wss_agent.sh"
     		//sh "chmod +x ./wss_agent.sh"
 		println "s ./wss_agent.sh -apiKey -userKey -product ${product} -project ${project} -wss.url https://app.whitesourcesoftware.com/agent -d ./. ${options}"
     	}
