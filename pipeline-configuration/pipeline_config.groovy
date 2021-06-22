f7d02d1d87f96a2fc33094663a68cb4821e6c4b2
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-22 16:16:22 -0400

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -3,17 +3,17 @@ jte{
 }
 
 keywords {
-	master 	= /^[Mm]aster$/
-	develop = /^[Dd]evelop(ment
-	main	= /^[Mm]ain$/
+	master 	= "/^[Mm]aster$/"
+	develop = "/^[Dd]evelop(ment"
+	main	= "/^[Mm]ain$/"
 }
 
 @merge libraries {
 	sdp {
 	  images {
-	    registry = "https://docker.pkg.github.com"
-	    repository = "boozallen/sdp-images"
-	    cred = "public-github"
+	    registry 	= "https://docker.pkg.github.com"
+	    repository 	= "boozallen/sdp-images"
+	    cred 	= "public-github"
 	    docker_args = "-u 0:0"
 	  }
         }
