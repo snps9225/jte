b0df60b94a78b458819a0d48d1574312598c5dfe
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-22 09:48:16 -0400

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -2,18 +2,26 @@ jte{
     allow_scm_jenkinsfile = true
 }
 
-libraries {
-	git {
-	   github {
-	      source_type = "github"
-	   }
-	} 
+
+@merge libraries {
+	sdp {
+	  images {
+	    registry = "https://docker.pkg.github.com"
+	    repository = "boozallen/sdp-images"
+	    cred = "public-github"
+	    docker_args = "-u 0:0"
+	  }
+        }
+	github {
+	   source_type = "github"
+	}
+
 	maven {
 	   mavenID = "maven"
 	}   
- 	whitesource {
+	whitesource {
 	   ws_product = "WhiteSource_Test" 
-    	   ws_project = "Test_jenkins"
-           ws_config  = "ws_mvn.config"
+	   ws_project = "Test_jenkins"
+	   ws_config  = "ws_mvn.config"
     }
 }
