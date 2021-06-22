81171cf316f8128edb9cfcee0dbc8bdb16f3fbd5
Karn Chahar <karn@synopsys.com>
2021-06-22 08:37:54 -0400

    Add config

---
+++ pipeline-configuration/pipeline_config.groovy
@@ -0,0 +1,15 @@
+jte{ 
+    allow_scm_jenkinsfile = true
+}
+
+libraries {
+	git 
+	maven {
+	   mavenID = "maven"
+	}   
+ 	whitesource {
+	   ws_product = "WhiteSource_Test" 
+    	   ws_project = "Test_jenkins"
+           ws_config  = "ws_mvn.config"
+    }
+}
