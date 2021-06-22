44e00f13dfc651e749dd073ebf02ebf4ae8724c7
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-22 11:30:50 -0400

    Update ws_sca.groovy

--- libraries/whitesource/steps/ws_sca.groovy
+++ libraries/whitesource/steps/ws_sca.groovy
@@ -2,11 +2,11 @@ void call(){
     node{
 		
     	stage("WhiteSource: Software Composition Analysis"){
-		String product = config.ws_product
-                String project = config.ws_project
-                String options = config.ws_cli
+		String product = config.Product
+                String project = config.Project
+                String custom_config = config.Custom_ConfigOptions
 
-		String configs = resource(config.ws_package)
+		String configs = resource(config.Package_Manager)
 	
 		sh 'curl -LO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar > wss-unified-agent.jar'
   		sh 'curl -LO https://github.com/whitesource/unified-agent-distribution/raw/master/standAlone/wss-unified-agent.config > wss-unified-agent.config'
@@ -18,7 +18,7 @@ void call(){
 		//["maven.downloadMissingDependencies=false","maven.projectNameFromDependencyFile=true"] -> wss-unified-agent.config
 	
 		withCredentials([string(credentialsId: 'api_key', variable: 'api_key'), string(credentialsId: 'user_key', variable: ' user_key')]) {
-			sh "java -jar wss-unified-agent.jar -apiKey 'api_key' -userKey 'user_key' -product ${product} -project ${product} -wss.url https://app.whitesourcesoftware.com/agent -d ./. ${options}"
+			sh "java -jar wss-unified-agent.jar -apiKey 'api_key' -userKey 'user_key' -product ${product} -project ${product} -wss.url https://app.whitesourcesoftware.com/agent -d ./."
                 }
 	}
     }
