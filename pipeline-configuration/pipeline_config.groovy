b9c693e661c0c3b44ec8999c6a5d65b78808c2c7
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-24 15:13:52 -0400

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -7,8 +7,8 @@ keywords {
 @merge libraries {
 	sdp {
 	  images {
-	    registry 	= "https://hub.docker.com" //"https://docker.pkg.github.com" //
-	    repository 	= "karnc/" //"boozallen/sdp-images" 
+	    registry 	= "https://registry.hub.docker.com" //"https://docker.pkg.github.com" //
+	    repository 	= "karnc" //"boozallen/sdp-images" 
 	    cred 	= "docker-hub"
 	    //docker_args = "-u 0:0"
 	  }
