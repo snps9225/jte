e3294fef2349313332fb1dca085dd9eb251a0b8c
Karn Chahar <karn@synopsys.com>
2021-06-24 12:52:31 -0400

    maven

---
+++ libraries/maven/maven.groovy
@@ -0,0 +1,18 @@
+void call() {
+
+} 
+
+void run(Map params = [:], ArrayList<String> phases) {
+    if (!config.mavenId) {
+        error "Must supply the installed Maven version's ID"
+    }
+    this.run(phases, params.get('goals', []) as ArrayList<String>, params.get('properties', [:]) as Map<String, String>, params.get('profiles', []) as ArrayList<String>)
+}
+
+// Run maven command with maven image pulled from registry
+void run(ArrayList<String> phases, ArrayList<String> goals, Map<String, String> properties, ArrayList<String> profiles) {
+    stage("Maven") {
+	// run mvn command in maven container
+        withCredentials(creds){
+            inside_sdp_image "maven:${tag}", {
+                unstash "workspace"
