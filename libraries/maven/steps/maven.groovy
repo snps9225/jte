f5ab154131037cbf2c841a17a1ee4d9f37b2cfa0
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-24 13:13:34 -0400

    Update maven.groovy

--- libraries/maven/steps/maven.groovy
+++ libraries/maven/steps/maven.groovy
@@ -12,10 +12,11 @@ void run(Map params = [:], ArrayList<String> phases) {
 // Run maven command with maven image pulled from registry
 // Run maven command with maven image pulled from registry
 void run(ArrayList<String> phases, ArrayList<String> goals, Map<String, String> properties, ArrayList<String> profiles) {
-        // run mvn command in maven container
-	String command = "echo "
-	if (properties) {
-		command = properties.get('image_tag')
-		sh command
-            }
+       stage("Maven") {  // run mvn command in maven container
+		String command = "echo DEBUG "
+		if (properties) {
+			command += properties.get('image_tag')
+	    	}
+	        sh command
+       }
 }
