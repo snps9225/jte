ce3a05c324d9b925448d257de14e3bcd3dfcba45
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-24 16:28:06 -0400

    Update maven.groovy

--- libraries/maven/steps/maven.groovy
+++ libraries/maven/steps/maven.groovy
@@ -7,40 +7,41 @@ void run(Map params = [:], ArrayList<String> phases) {
 
 // Run maven with the image pulled from registry
 void run(ArrayList<String> phases, ArrayList<String> goals, Map<String, String> properties, ArrayList<String> profiles) {
-       stage("Maven") {  
+       stage("Maven: Build") {  
+	        unstash "workspace"
+	        
+	        String command = "mvn "
 	        String tag = ""
+	       
 	        if (!config.image_tag) {
         		tag = "3.8.1-openjdk-8"
     		}
 	        else {       
 	        	tag = config.image_tag
 		}
-		inside_sdp_image "maven:${tag}", {
-		    unstash "workspace"
-		    String command = "mvn "
-		    if (!phases) {
+		if (!phases) {
 			error "Must supply phase for Maven"
-		    }
-		    phases.each { phase -> command += "${phase} "}
-
-		    if (goals) {
+		}
+		phases.each { phase -> command += "${phase} "}
+		if (goals) {
 			goals.each { goal -> command += "${goal} " }
-		    }
+		}
 
-		    if (properties) {
+		if (properties) {
 			properties.each { propertyName, value -> command += "-D${propertyName} "
-			    if (value != null) {
+			if (value != null) {
 				command += "= ${value} "
-			    }
 			}
-		    }
-
-		    if (profiles) {
+		}
+		
+		if (profiles) {
 			command += "-P"
 			String joined = profiles.join(",")
 			command += joined
-		    }
+		}
+	       
+		inside_sdp_image "maven:${tag}", {
 	            sh command
 		}
-       }
+      }
 }
