974d7c9275877a01e0be2e73d313794e2ab5e5ab
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-24 16:36:53 -0400

    Update maven.groovy

--- libraries/maven/steps/maven.groovy
+++ libraries/maven/steps/maven.groovy
@@ -8,8 +8,7 @@ void run(Map params = [:], ArrayList<String> phases) {
 // Run maven with the image pulled from registry
 void run(ArrayList<String> phases, ArrayList<String> goals, Map<String, String> properties, ArrayList<String> profiles) {
        stage("Maven: Build") {  
-	        
-	        
+	       	        
 	        String command = "mvn "
 	        String tag = ""
 	       
@@ -43,7 +42,6 @@ void run(ArrayList<String> phases, ArrayList<String> goals, Map<String, String> 
 		}
 	       
 		inside_sdp_image "maven:${tag}", {
-			unstash "workspace"
 	            	sh command
 		}
       }
