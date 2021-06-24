366dcaa8d80d1df3711dbe334cfe120f1da51051
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-24 13:29:39 -0400

    Update maven.groovy

--- libraries/maven/steps/maven.groovy
+++ libraries/maven/steps/maven.groovy
@@ -13,10 +13,25 @@ void run(Map params = [:], ArrayList<String> phases) {
 // Run maven command with maven image pulled from registry
 void run(ArrayList<String> phases, ArrayList<String> goals, Map<String, String> properties, ArrayList<String> profiles) {
        stage("Maven") {  // run mvn command in maven container
-		String command = "echo DEBUG "
+		String tag = ""
 		if (properties) {
-			command += properties.get('image_tag')
+			tag = properties.get('image_tag')
 	    	}
-	        sh command
-       }
+	       
+	       	withCredentials(creds){
+			inside_sdp_image "maven:${tag}", {
+			unstash "workspace"
+			String command = "mvn "
+		    
+		    if (!phases) {
+			error "Must supply phase for Maven"
+		    }
+		    phases.each { phase -> command += "${phase} "}
+
+		    if (goals) {
+			goals.each { goal -> command += "${goal} " }
+		    }
+		    sh command
+        }
+    }
 }
