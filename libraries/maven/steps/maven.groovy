c6c001e4ff6f28702a0ca06e714161f53f575522
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-24 14:54:11 -0400

    Update maven.groovy

--- libraries/maven/steps/maven.groovy
+++ libraries/maven/steps/maven.groovy
@@ -3,36 +3,26 @@ void call() {
 } 
 
 void run(Map params = [:], ArrayList<String> phases) {
-    if (!config.mavenId) {
-        error "Must supply the installed Maven version's ID"
-    }
     this.run(phases, params.get('goals', []) as ArrayList<String>, params.get('properties', [:]) as Map<String, String>, params.get('profiles', []) as ArrayList<String>)
 }
 
 // Run maven command with maven image pulled from registry
-// Run maven command with maven image pulled from registry
 void run(ArrayList<String> phases, ArrayList<String> goals, Map<String, String> properties, ArrayList<String> profiles) {
-       stage("Maven") {  // run mvn command in maven container
-		String tag = ""
-		if (properties) {
-			tag = properties.get('image_tag')
-	    	}
-	       
-	       	//withCredentials(creds){
-			inside_sdp_image "maven:${tag}", {
-			unstash "workspace"
-			String command = "mvn "
-		    
-			    if (!phases) {
-				error "Must supply phase for Maven"
-			    }
-			    phases.each { phase -> command += "${phase} "}
+       stage("Maven") {  
+		withCredentials(cred){
+		inside_sdp_image "maven:${config.maven_tag}", {
+		//unstash "workspace"
+		String command = "mvn "
 
-			    if (goals) {
-				goals.each { goal -> command += "${goal} " }
-			    }
-			    sh command
-			}
-        	//}
-    	}
+		    if (!phases) {
+			error "Must supply phase for Maven"
+		    }
+		    phases.each { phase -> command += "${phase} "}
+
+		    if (goals) {
+			goals.each { goal -> command += "${goal} " }
+		    }
+		    //sh command
+		}
+       }
 }
