0dbdde49d91cc2f19a275fa4d94514cf3c685a8a
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-07-14 08:03:57 -0400

    Add files via upload

---
+++ libraries/checkmarx/library_config.groovy
@@ -0,0 +1,13 @@
+fields{ 
+  required {
+	ProjectName = String
+	Language = String
+  } 
+  optional{
+	Preset = String
+	LocationPath = String
+	SASTHigh = String
+	SASTMedium = String
+	SASTLow = String
+  } 
+}
---
+++ libraries/checkmarx/steps/checkmarx.groovy
@@ -0,0 +1,107 @@
+void call()
+{
+
+}
+
+void run(String lang)
+{
+	stage("Checkmarx : Static Analysis") 
+	{
+
+		String project = ""
+		String language = ""
+		String preset = ""
+		String LocationPath= ""
+		String SASTHigh = ""
+		String SASTMedium = ""
+		String script = ""
+		String report = "cx_output.xml"
+		String CxServer="https://checkmarx.corp.n-able.com"
+		String LocationType="folder"
+		
+		String report_location = "/opt/CxConsolePlugin/Checkmarx/Reports/"
+		
+		if (!config.ProjectName) 
+		{
+        		error "Error : Must provide ProjectName"
+    		}
+		project = config.ProjectName
+		project = project.replace("\\", "\\\\")
+	        
+		if (!config.Language) 
+		{
+        		error "Error : Must provide programming language name"
+    		}
+		lang = config.Language
+		lang=lang.toLowerCase()
+		
+		preset = choose_preset(lang)
+		if(preset==null)
+		{
+			error "Error : Unsupported language template"
+		}
+		
+		if(!config.SASTHigh)
+		{
+			SASTHigh="5"
+		}
+		else
+		{
+			SASTHigh=config.SASTHigh
+		}
+		if(!config.SASTMedium)
+		{
+			SASTMedium="5"
+		}
+		else
+		{
+			SASTMedium=config.SASTMedium
+		}
+		
+		script = '/opt/CxConsolePlugin/runCxConsole.sh scan -v'
+		script = script + ' -ProjectName '+ '\\"'+ project +'\\"'
+		script = script + ' -CxServer '+'\\"'+CxServer+'\\"'
+		script = script + ' -LocationType '+'\\"'+LocationType+'\\"'
+		script = script + ' -SASTHigh '+'\\"'+ SASTHigh +'\\"'
+		script = script + ' -SASTMedium '+'\\"'+SASTMedium+'\\"'
+		script = script + ' -Preset '+ '\\"'+preset+'\\"'
+		script = script + ' -ReportXML '+'\\"'+report+'\\"'
+		script = script + ' -Incremental'
+		
+		// Fix the Dockerfile
+		// Archive the results
+		
+         	inside_sdp_image "checkmarx_test:latest", {
+         	 	
+			dir("${WORKSPACE}")
+			{
+
+				script = script + ' -LocationPath '+'\\"'+ "${WORKSPACE}" +'\\"'
+				withChecks('Checkmarx Scan') 
+				{
+					withCredentials([usernamePassword(credentialsId: 'Cx-Access', passwordVariable: 'pass', usernameVariable: 'uname')]) 
+					{
+						script = script + ' -CxUser '+'\\"'+"$uname"+'\\"'+' -CxPassword '+'\\"'+"$pass"+'\\"'
+						def statusCode = sh script:script, returnStatus:true
+						if(statusCode==0 || statusCode>5)
+						{
+							//stash includes: '/opt/CxConsolePlugin/Checkmarx/Reports/cx_output.xml', name: 'Cxscan'
+						}
+					}
+				}
+			}
+		}
+		
+	}
+}
+
+String choose_preset(String lang)
+{
+	switch(lang)
+	{
+		case "java":
+			return '\"Checkmarx Default\"'
+		default:
+			return null
+	}
+}
