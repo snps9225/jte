ecf876c7f089ff07c29cd92dbee40fefc0812dd6
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-07-14 08:07:01 -0400

    Update maven.groovy

--- libraries/maven/steps/maven.groovy
+++ libraries/maven/steps/maven.groovy
@@ -7,46 +7,111 @@ void run(Map params = [:], ArrayList<String> phases) {
 
 // Run maven with the image pulled from registry
 void run(ArrayList<String> phases, ArrayList<String> goals, Map<String, String> properties, ArrayList<String> profiles) {
-       stage("Maven: Build") {  
-	       	        
-	        String command = "mvn "
-	        String tag = ""
-	        String image_name = "build-tools/maven_"
-	       
-	        if (!config.maven_version) {
-        		tag = "3.8.1"
+       stage("Maven: Build") 
+       {  
+	     	
+	     	// Parsing AWS CodeArtifact	
+	     	String aws_configure_cmd = null
+	     	String aws_codeartifact_cmd = null 
+	        String codeArtifactOutput = null
+	  	
+	     	if (config.aws.defaultregion) 
+	     	{
+        		aws_configure_cmd = "aws configure set default.region "+config.aws.defaultregion
     		}
-	        else {       
-	        	tag = config.maven_version
+    		
+    		if(config.aws.codeartifact_action)
+    		{
+    			aws_codeartifact_cmd = "aws codeartifact " + config.aws.codeartifact_action
+    		}
+    		
+    		if(config.aws.codeartifact_domain)
+    		{
+    			aws_codeartifact_cmd = aws_codeartifact_cmd + " --domain "+config.aws.codeartifact_domain
+    		}
+    		
+    		if(config.aws.codeartifact_domain_owner)
+    		{
+    			aws_codeartifact_cmd = aws_codeartifact_cmd + " --domain-owner "+config.aws.codeartifact_domain_owner
+    		}
+    		
+    		if(config.aws.codeartifact_query)
+    		{
+    			aws_codeartifact_cmd = aws_codeartifact_cmd + " --query "+config.aws.codeartifact_query
+    		}
+    		
+    		if(config.aws.codeartifact_output)
+    		{
+    			aws_codeartifact_cmd = aws_codeartifact_cmd + " --output "+config.aws.codeartifact_output
+    		}
+    		
+	     	
+	        // Parsing maven code
+	        String mvn_command = "mvn "
+	        String tag = ""
+	        String image_name = "build-tools_maven"
+	       
+	        if (!config.maven.version) {
+        		tag = "381"
+    		}
+	        else {  
+	        	tag = config.maven.version
 		}
-	        tag = tag.replaceAll("_", ".")
-	        image_name = image_name+tag 
-	        println image_name
-		if (!phases) {
+	        tag = tag.replaceAll("\\.", "")
+	        image_name = image_name+tag
+	        //image_name = "build-tools_maven381"
+		
+	        if (!phases) {
 			error "Must supply phase for Maven"
 		}
-		phases.each { phase -> command += "${phase} "}
+		phases.each { phase -> mvn_command += "${phase} "}
 		
 	        if (goals) {
-			goals.each { goal -> command += "${goal} " }
+			goals.each { goal -> mvn_command += "${goal} " }
 		}
 
 		if (properties) {
-			properties.each { propertyName, value -> command += "-D${propertyName} "
+			properties.each { propertyName, value -> mvn_command += "-D${propertyName} "
 				if (value != null) {
-					command += "= ${value} "
+					mvn_command += "= ${value} "
 				}
 			}
 		}
 		
 		if (profiles) {
-			command += "-P"
+			mvn_command += "-P"
 			String joined = profiles.join(",")
-			command += joined
+			mvn_command += joined
 		}
 	       
-		inside_sdp_image "${image_name}", {
-	            	sh command
+	       if(config.maven.modules)
+	       {
+		      mvn_command += "-pl "
+		      mvn_command += config.maven.modules+" "
+	       }
+	       
+	       if(config.maven.settings)
+	       {
+		      mvn_command += "-s "
+		      mvn_command += config.maven.settings+" "
+	       }
+	       
+		inside_sdp_image "maven:latest", {
+			
+			echo aws_configure_cmd
+			echo aws_codeartifact_cmd
+			echo mvn_command
+			if(aws_configure_cmd!=null)
+			{
+				//sh aws_configure_cmd
+			}
+			if(aws_codeartifact_cmd!=null)
+			{
+				//codeArtifactOutput = sh(script: aws_codeartifact_cmd, returnStdout: true).trim()
+			}
+			
+			//sh "export CODEARTIFACT_AUTH_TOKEN=${codeArtifactOutput} && ${mvn_command} && printenv | sort"
+			
 		}
       }
 }
