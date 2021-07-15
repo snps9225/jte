4ce27c9404db1c741b277c22ab37d891f42f56cb
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-07-15 16:09:20 -0400

    Update maven.groovy

--- libraries/maven/steps/maven.groovy
+++ libraries/maven/steps/maven.groovy
@@ -100,7 +100,7 @@ void run(ArrayList<String> phases, ArrayList<String> goals, Map<String, String> 
 			echo mvn_command
 			sh mvn_command
 			phases.each {
-				if(${phase} == "test") {
+				if("${phase}" == "test") {
 					echo "run JUNIT"
 				}    
 			}
