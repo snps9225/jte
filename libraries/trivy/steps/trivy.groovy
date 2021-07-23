527c63c2548e8fa8d588634e7ea0d6a65a22a992
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-07-23 18:40:55 -0400

    Update trivy.groovy

--- libraries/trivy/steps/trivy.groovy
+++ libraries/trivy/steps/trivy.groovy
@@ -4,11 +4,11 @@ void call() {
 			String image_name = ""
 			String script = "" 
 			image_name = config.Image_Name 
-			script = 'docker build -t ' + image_name + ' .'
+			script = 'docker build -t ' + image_name + ' . > trivy-result.txt'
 			
 			//sh "test -w ./Dockerfile && echo \"Dockerfile exists.\" || echo \"Dockerfile does not exist.\""
 			sh "test -w ./Dockerfile && echo \"\n\nUSER root\" >> Dockerfile || echo \"\""
-			sh "test -w ./Dockerfile && echo \"\nRUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --exit-code 1 --no-progress /\" >> Dockerfile || echo \"\""
+			sh "test -w ./Dockerfile && echo \"\nRUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --ignore-unfixed --severity HIGH,CRITICAL --exit-code 0 --no-progress /\" >> Dockerfile || echo \"\""
 			//sh "test -w ./Dockerfile && sh ${script} || echo \"\""
 			
 			
