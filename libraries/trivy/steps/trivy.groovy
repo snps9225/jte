291bdcceffa8d86bce8a44939979de1078bf4127
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-07-23 16:57:29 -0400

    Update trivy.groovy

--- libraries/trivy/steps/trivy.groovy
+++ libraries/trivy/steps/trivy.groovy
@@ -5,16 +5,17 @@ void call() {
 		boolean flag = false
 		image_name = config.Image_Name 
 
-		unstash "workspace" 
+		dir("${WORKSPACE}") { 
 
-		sh "[ -f ./Dockerfile ] && echo \"${configs}\"=true && echo \"Dockerfile exists.\""
+			sh "[ -f ./Dockerfile ] && echo \"${flag}\"=true && echo \"Dockerfile exists.\""
 
-		if(flag==true) {
-		    sh "echo \"USER root\" >> Dockerfile"
-		    sh "echo \"RUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --exit-code 1 --no-progress /\" >> Dockerfile"
+			if(flag==true) {
+			    sh "echo \"USER root\" >> Dockerfile"
+			    sh "echo \"RUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --exit-code 1 --no-progress /\" >> Dockerfile"
+			}
+
+			script = 'docker build -t ' + image_name + ' .'  
+			sh script
 		}
-
-		script = 'docker build -t ' + image_name + ' .'  
-		sh script 
     	}
 }
