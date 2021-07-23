b73d6696dd9ad8d36884004955b62336f8ca4fda
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-07-23 16:47:46 -0400

    Update trivy.groovy

--- libraries/trivy/steps/trivy.groovy
+++ libraries/trivy/steps/trivy.groovy
@@ -1,22 +1,20 @@
 void call() {
-	stage("Trivy: Image Scan") 
-	{
-
+	stage("Trivy: Image Scan") {
 		String image_name = ""
-        String script = "" 
-        boolean flag = false
-        image_name = config.Image_Name 
+		String script = "" 
+		boolean flag = false
+		image_name = config.Image_Name 
 
-        unstash "workspace" 
+		unstash "workspace" 
 
-        sh "[ -f ./Dockerfile ] && echo \"${configs}\"=true && echo \"Dockerfile exists.\""
+		sh "[ -f ./Dockerfile ] && echo \"${configs}\"=true && echo \"Dockerfile exists.\""
 
-        if(flag==true) {
-            sh "echo \"USER root\" >> Dockerfile"
-            sh "echo \"RUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --exit-code 1 --no-progress /\" >> Dockerfile"
-        }
+		if(flag==true) {
+		    sh "echo \"USER root\" >> Dockerfile"
+		    sh "echo \"RUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --exit-code 1 --no-progress /\" >> Dockerfile"
+		}
 
-        script = 'docker build -t ' + image_name + ' .'  
-        sh script 
-    }
+		script = 'docker build -t ' + image_name + ' .'  
+		sh script 
+    	}
 }
