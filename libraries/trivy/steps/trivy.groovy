a4b28b4c26961f14f7d954df3939c165ef1bf726
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-07-23 19:04:18 -0400

    Update trivy.groovy

--- libraries/trivy/steps/trivy.groovy
+++ libraries/trivy/steps/trivy.groovy
@@ -2,13 +2,18 @@ void call() {
 	stage("Trivy: Image Scan") {
 		node {
 			String image_name = ""
-			String script = "" 
+			String script = ""
+			String report_format = "table"
+			int break_build = 0
 			image_name = config.Image_Name 
+			report_format = config.Report_Format
+			break_build = config.Break_Build
+			
 			script = 'docker build -t ' + image_name + ' . > trivy-result.txt'
 			
 			//sh "test -w ./Dockerfile && echo \"Dockerfile exists.\" || echo \"Dockerfile does not exist.\""
 			sh "test -w ./Dockerfile && echo \"\n\nUSER root\" >> Dockerfile || echo \"\""
-			sh "test -w ./Dockerfile && echo \"\nRUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --ignore-unfixed --severity HIGH,CRITICAL --exit-code 0 --no-progress /\" >> Dockerfile || echo \"\""
+			sh "test -w ./Dockerfile && echo \"\nRUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --ignore-unfixed --severity HIGH,CRITICAL --format \"${report_format}\" --exit-code \"${break_build}\" --no-progress /\" >> Dockerfile || echo \"\""
 			//sh "test -w ./Dockerfile && sh ${script} || echo \"\""
 
 			sh script
