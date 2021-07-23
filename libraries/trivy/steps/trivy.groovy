a6dd838a81fad1c87d442631e55795b69530279d
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-07-23 17:53:51 -0400

    Update trivy.groovy

--- libraries/trivy/steps/trivy.groovy
+++ libraries/trivy/steps/trivy.groovy
@@ -6,10 +6,10 @@ void call() {
 			image_name = config.Image_Name 
 			script = 'docker build -t ' + image_name + ' .'
 			
-			sh "test -w ./Dockerfile2 && echo \"Dockerfile exists.\" || echo \"Dockerfile does not exist.\""
-			sh "test -w ./Dockerfile2 && echo \"\n\nUSER root\" >> Dockerfile || echo \"\""
-			sh "test -w ./Dockerfile2 && echo \"\nRUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --exit-code 1 --no-progress /\" >> Dockerfile || echo \"\""
-			sh "test -w ./Dockerfile2 && sh ${script} || echo \"\""
+			sh "test -w ./Dockerfile && echo \"Dockerfile exists.\" || echo \"Dockerfile does not exist.\""
+			sh "test -w ./Dockerfile && echo \"\n\nUSER root\" >> Dockerfile || echo \"\""
+			sh "test -w ./Dockerfile && echo \"\nRUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --exit-code 1 --no-progress /\" >> Dockerfile || echo \"\""
+			sh "test -w ./Dockerfile && sh ${script} || echo \"\""
 			
 			
 			/*if(file.exists()) {
