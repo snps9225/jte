919c46f1166180d2004ecf54afcabb8671c923ad
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-07-30 11:50:12 -0400

    Update trivy.groovy

--- libraries/trivy/steps/trivy.groovy
+++ libraries/trivy/steps/trivy.groovy
@@ -11,7 +11,7 @@ void call() {
 			
 			script = 'docker build -t ' + image_name + ' . > trivy-result.txt'
 			
-			sh "test -w ./Dockerfile && cp Dockerfile Dockerfile_copy || echo \"Dockerfile does not exist.\""
+			sh "test -w ./Dockerfile && cp Dockerfile Dockerfile_copy || echo \"Dockerfile does not exist, build will fail.\""
 			sh "test -w ./Dockerfile && echo \"\n\nUSER root\" >> Dockerfile || echo \"\""
 			sh "test -w ./Dockerfile && echo \"\nRUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin && trivy filesystem --ignore-unfixed --severity HIGH,CRITICAL --format \"${report_format}\" --exit-code \"${break_build}\" --no-progress /\" >> Dockerfile || echo \"\""
 			//sh "test -w ./Dockerfile && sh ${script} || echo \"\""
