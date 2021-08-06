70277a264bef25812fafcb38b7706f7860f58519
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-08-06 09:15:29 -0400

    Update pipeline_config.groovy

--- test/pipeline_config.groovy
+++ test/pipeline_config.groovy
@@ -14,13 +14,23 @@ libraries {
         }
 	
 	@merge checkmarx {
-		CxServer = "https://checkmarx.corp.n-able.com"
-	}
+	   	CxServer = "https://checkmarx.corp.n-able.com"
+		@override ProjectName = "CxServer\\SP\\Solarwinds\\MSP_N-Central\\General_Scan"
+		@override Language = "Java"
+		@override CxCred   = "Cx-Access"
+   	}
 	
 	@merge whitesource {
 		Api_Key = "api_key"
 		Wss_Url = "https://app.whitesourcesoftware.com/agent"
-	}
+		@override Product  = "WhiteSource_General_Scan"
+		@override Project  = "General_Scan"
+		@override User_Key = "user_key"
+   	}
+
+	@merge trivy {
+        	@override Image_Name  = "vuln-check:image"
+   	}
 	
 	/*git {
 	   	github
