b7b99a7acba5b874bd9ddfe62e99491a65bfdd95
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-08-16 11:10:32 -0400

    Create iac_scan.groovy

---
+++ libraries/checkov/steps/iac_scan.groovy
@@ -0,0 +1,67 @@
+
+void call() {
+	stage("IaC Scan: Checkov") {
+		node {
+            String format      = ""
+            String file        = "" 
+            String framework   = "" 
+            String baseline    = "" 
+            String incremental = ""  
+            String script      = ""
+            int flag           = 0
+      
+			      format 	    = config.Format
+            file        = config.File
+            framework   = config.Framework
+            baseline    = config.CreateBaseline 
+            incremental = config.UseBaseline 
+			      break_build	= config.Break_Build
+			
+            // unstash name: 'maven_build' 
+
+            test = "find . -type f -name \'\*.\*\' | sed \'s|.\*\.||\' | sort -u > presence"
+            sh test
+
+            def lines = presence.readLines()
+            lines.each { String line ->
+                if(line.contins("tf") || line.contins("yaml") || line.contins("yml")) {
+                    flag = 1
+                }
+            }
+
+            if(flag == 1) { 
+
+                if(!config.Format) {
+                    format = "json"
+                    println "No report format was provided. Default is: " + format
+                }
+
+                if(!config.File) {
+                  file = "iac-scan-results.json"  
+                  println "No report name was provided. Default is: " + file
+                }
+
+                if(!config.Framework) {
+                    framework = "all"
+                    println "No specific framework was selected. Default is: " + framework
+                }
+                
+                script = 'checkov --directory .'
+                script = script + ' --output ' + format + ' > ' + file
+                script = script + ' --framework ' + framework 
+              
+                if (!config.CreateBaseline || create_baseline.equals("yes")) {
+                    script = script + ' --create-baseline'
+                }
+
+                if (use_baseline.equals("yes")) {
+                    script = script + ' --baseline'
+                }
+
+                sh script
+            }
+            else 
+                println "Info: IaC files do not exist. Checkov scanning will be skipped."
+        }
+    }
+}
