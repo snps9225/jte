989bd525f488744fb752e1a61d97d5fb27ae7e7c
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-08-16 11:22:13 -0400

    Create library_config.groovy

---
+++ libraries/checkov/steps/library_config.groovy
@@ -0,0 +1,10 @@
+fields {
+  optional {
+    Format    = String
+    File      = String
+    Framework = String
+    Create_Baseline = String
+    Use_Baseline    = String
+    Break_Build     = String
+  }
+}
