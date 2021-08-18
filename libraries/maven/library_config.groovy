9a5eeb699b8526eb866c47c0a650944eb4eeb485
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-08-18 16:21:57 -0400

    Update library_config.groovy

--- libraries/maven/library_config.groovy
+++ libraries/maven/library_config.groovy
@@ -1,21 +1,26 @@
 fields { 
-  optional {
-	  maven
-	  {
-	  	version = String
-		settings = String
-		modules = String
-		stage_name = String
-		artifact_version = String  
-	  }
-	  aws
-	  {
-	  	defaultregion = String
-	 	codeartifact_action = String
-	  	codeartifact_domain = String
-	  	codeartifact_domain_owner = String
-	  	codeartifact_query = String
-	  	codeartifact_output = String
-	  }
-  }
+	optional {
+		maven {	
+			Phases = ArrayList<String> 
+			Goals = ArrayList<String>
+			Properties = Map<String, String>
+			Profiles = ArrayList<String> 
+			Stage = String
+				
+			version = String
+			settings = String
+			modules = String
+			stage_name = String
+			artifact_version = String  
+		}
+		
+		aws {
+			defaultregion = String
+			codeartifact_action = String
+			codeartifact_domain = String
+			codeartifact_domain_owner = String
+			codeartifact_query = String
+			codeartifact_output = String
+		}
+	}
 }
