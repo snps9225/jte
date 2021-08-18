8b84883a6b70cb065ad883d84d7f79cfd3561b2c
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-08-18 16:15:59 -0400

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,14 +1,19 @@
 void call() {
 	stage("Build: Maven") {
 		ArrayList<String> phases
-    ArrayList<String> goals
-    Map<String, String> properties
-    ArrayList<String> profiles
-    String stage_name = ""
+		ArrayList<String> goals
+		Map<String, String> properties
+		ArrayList<String> profiles
+		String stage_name = ""
+    
+		phases	= config.maven.Phases
+		goals	= config.maven.Goals 
+		properties = config.maven.Properties
+		profiles = config.maven.Profile
+		stage_name = config.maven.Stage
     
     
-    
-    maven.run(phases,goals,properties,profiles,stage_name)
-  }
+		maven.run(phases,goals,properties,profiles,stage_name)
+  	}
 }
       
