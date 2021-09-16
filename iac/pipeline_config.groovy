e7d612f1ba5d65a2ee41beaf847d68fc4218a2c1
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-09-16 14:27:44 -0400

    Create pipeline_config.groovy

---
+++ iac/pipeline_config.groovy
@@ -0,0 +1,33 @@
+
+@merge libraries {
+	@override sdp {
+		images {
+			registry 	= "https://registry.hub.docker.com"
+			repository 	= "karnc" 
+			cred 		= "docker-hub"
+	  	}
+  }
+	
+	@merge checkmarx {
+	  CxServer = "https://checkmarx.corp.n-able.com"
+		@override ProjectName = "CxServer\\SP\\Solarwinds\\MSP_N-Central\\General_Scan"
+		@override Language = "Java"
+		@override CxCred   = "Cx-Access"
+  }
+	
+	@merge whitesource {
+		Api_Key = "api_key"
+		Wss_Url = "https://app.whitesourcesoftware.com/agent"
+		@override Product  = "WhiteSource_General_Scan"
+		@override Project  = "General_Scan"
+		@override User_Key = "user_key"
+  }
+
+	@override trivy
+	
+	@override checkov
+
+	github {
+     		source_type = "github"
+  	}
+}
