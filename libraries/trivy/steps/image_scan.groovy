ae286126e76f0acc9683da969f4e5f6fa8de5940
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-09-16 11:53:24 -0400

    Update image_scan.groovy

--- libraries/trivy/steps/image_scan.groovy
+++ libraries/trivy/steps/image_scan.groovy
@@ -13,48 +13,49 @@ void call() {
 			break_build	= config.Break_Build
 			severity 	= config.Severity
 			
-			unstash name: 'maven_build' 
+			dir("${WORKSPACE}") {
 			
-			flag = sh(script: '''
-			touch presence
-			find . -type f -name 'Dockerfile' | sed \'s|\\(.*\\)/.*|\\1|\' | sort -u >> presence
-			[ -s presence ] && echo 0 || echo 1
-			''', returnStdout: true).trim()
+				flag = sh(script: '''
+				touch presence
+				find . -type f -name 'Dockerfile' | sed \'s|\\(.*\\)/.*|\\1|\' | sort -u >> presence
+				[ -s presence ] && echo 0 || echo 1
+				''', returnStdout: true).trim()
 
-			if(flag.equals("0")) {
+				if(flag.equals("0")) {
 
-				if(!config.Image_Name) {
-					image_name = "vuln-scan:trivy"
-					println "No image name was provided. Default image name to be scanned is, " + image_name
+					if(!config.Image_Name) {
+						image_name = "vuln-scan:trivy"
+						println "No image name was provided. Default image name to be scanned is, " + image_name
+					}
+
+					if(!config.Break_Build) {
+						println "Selected default break build setting: Do not break build"
+						break_build = 0
+					}
+
+					if(!config.Severity) {
+						println "Selected default severity setting: High and Critical"
+						severity = "HIGH,CRITICAL"
+					}
+
+					String [] lines = readFile("presence").split(System.getProperty("line.separator"));
+
+					while (index < lines.length){
+						script = "docker build -t \"${image_name}\" \"${lines[index]}\""
+						def statusCode = sh script:script, returnStatus:true
+
+						println "trivy image --format json -o image-scan-${scanID}-.json --ignore-unfixed --no-progress --exit-code ${break_build} --severity ${severity} ${image_name}"
+						sh "docker rmi \"${image_name}\""
+
+						//archiveArtifacts artifacts: "**/trivy-scan.json"
+						index++
+						scanID++
+					}
+					sh 'rm presence'
 				}
-
-				if(!config.Break_Build) {
-					println "Selected default break build setting: Do not break build"
-					break_build = 0
-				}
-
-				if(!config.Severity) {
-					println "Selected default severity setting: High and Critical"
-					severity = "HIGH,CRITICAL"
-				}
-				
-				String [] lines = readFile("presence").split(System.getProperty("line.separator"));
-
-				while (index < lines.length){
-					script = "docker build -t \"${image_name}\" \"${lines[index]}\""
-					def statusCode = sh script:script, returnStatus:true
-
-					println "trivy image --format json -o image-scan-${scanID}-.json --ignore-unfixed --no-progress --exit-code ${break_build} --severity ${severity} ${image_name}"
-					sh "docker rmi \"${image_name}\""
-					
-					//archiveArtifacts artifacts: "**/trivy-scan.json"
-					index++
-					scanID++
-				}
-				sh 'rm presence'
+				else
+					println "Info: Dockerfile does not exist. Trivy scanning will be skipped."
 			}
-			else
-				println "Info: Dockerfile does not exist. Trivy scanning will be skipped."
 		}
     	}
 }
